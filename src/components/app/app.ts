1ea27186e2f93d75d8d2a7c050945795b90230af
shzfas <yuraka200486@gmail.com>
2022-12-12 17:37:07 +0600

    refactor: ts transition app main component

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -2,15 +2,17 @@ import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
 
 class App {
+    controller: AppController;
+    view: AppView;
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
     }
 
-    start() {
+    start(): void {
         document
             .querySelector('.sources')
-            .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
+            .addEventListener('click', (e: Event) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
         this.controller.getSources((data) => this.view.drawSources(data));
     }
 }
