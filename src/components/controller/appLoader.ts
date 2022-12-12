1009a54456d450ddb357bd048553f67c60b9e8d3
shzfas <yuraka200486@gmail.com>
2022-12-12 17:28:43 +0600

    feat: add/fix API key

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '7ecf71f95f0f428c89a2425bcadb0c19', // получите свой ключ https://newsapi.org/
         });
     }
 }
