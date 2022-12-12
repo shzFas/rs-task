969ba3c4050e3ab0ad4c033a48cad2482fad0865
shzfas <yuraka200486@gmail.com>
2022-12-12 17:29:31 +0600

    refactor: ts transition news component

---
+++ src/types/index.d.ts
@@ -0,0 +1,10 @@
+type NewsItem = {
+    author: string;
+    content: string;
+    description: string;
+    publishedAt: string;
+    source: { id: string; name: string };
+    title: string;
+    url: string;
+    urlToImage: string;
+};
