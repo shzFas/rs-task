3c4a7c4fcaf976a0d92f2564dae636b60f63eafd
shzfas <yuraka200486@gmail.com>
2022-12-12 17:33:50 +0600

    refactor: ts transition appview component

--- src/components/view/appView.ts
+++ src/components/view/appView.ts
@@ -2,20 +2,22 @@ import News from './news/news';
 import Sources from './sources/sources';
 
 export class AppView {
-    constructor() {
-        this.news = new News();
-        this.sources = new Sources();
-    }
+  news: News;
+  sources: Sources;
+  constructor() {
+    this.news = new News();
+    this.sources = new Sources();
+  }
 
-    drawNews(data) {
-        const values = data?.articles ? data?.articles : [];
-        this.news.draw(values);
-    }
+  drawNews(data: DataNewsInterface) {
+    const values = data?.articles ? data?.articles : [];
+    this.news.draw(values);
+  }
 
-    drawSources(data) {
-        const values = data?.sources ? data?.sources : [];
-        this.sources.draw(values);
-    }
+  drawSources(data: DataSourcesInterface) {
+    const values = data?.sources ? data?.sources : [];
+    this.sources.draw(values);
+  }
 }
 
 export default AppView;
